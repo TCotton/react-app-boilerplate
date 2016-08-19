11b258c4bd85eb6819b2f05ce6c695d878796809
Andy Walpole <me@andywalpole.me>
2016-08-19 16:25:52 +0100

    minor addition to card js

--- app/Card.js
+++ app/Card.js
@@ -3,17 +3,48 @@ import CheckList from './CheckList';
 
 class Card extends React.Component {
 
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      showDetails: false
+    };
+
+  }
+
   render() {
-    return (
-      <div className="card">
-        <div className="card__title">{this.props.title}</div>
+
+    var cardDetails;
+
+    if (this.state.showDetails) {
+
+      cardDetails = (
+
         <div className="card__details">
           {this.props.description}
-          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+          <CheckList cardId={this.props.id} tasks={this.props.tasks}/>
         </div>
-      </div>
-    );
+
+      );
+
+    }
+
+    return (
+
+
+
+    )
+
   }
+
 }
+
+/* <div className="card">
+ <div className="card__title">{this.props.title}</div>
+ <div className="card__details">
+ {this.props.description}
+ <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+ </div>
+ </div>*/
 
 export default Card;
